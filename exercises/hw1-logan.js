b1c971538dbe156cc712ec7002ba7da8108ee5eb
Logan Blackstad <logan.blackstad@gmail.com>
2020-10-05 23:24:15 -0400

    added a .js with a sample of the hw1 solution

---
+++ exercises/hw1-logan.js
@@ -0,0 +1,63 @@
+// Intro to Javascript
+
+/*
+
+Define the following
+
+Statement:
+a statement is ...
+
+Expression:
+an expression is ...
+
+Token:
+a token is ...
+
+Identifier:
+an identifier is ...
+
+*/
+
+/*
+In four sentences or less, explain the following
+What is the difference between =, ==, and ===?
+= is used to ...
+== is used to ...
+=== is used to ...
+
+What is the difference between declaration and initialization?
+the differnce is ...
+
+*/
+
+// Exercise 
+
+
+//Create a program that contains the following. Use values of your choosing that satisfy the conditions given.
+
+// a). Alpha is a number that is > Beta. Beta is a number < Charlie. Charlie is a string. In the program evaluate if
+// Alpha and Beta have equal values, then determine if Beta and Charlie are equal values and are the same type. If
+// Alpha is not less that Beta, log a message. If Beta and Charlie are of the same value and type, log a message.
+
+var alpha = 5
+var beta = 4
+var charlie = 'charlie'
+
+console.log(alpha > beta)  // returns true
+
+if (alpha < beta) {
+  console.log("")
+}
+
+// hint:  == and === are comparison operators
+
+
+// b). Add a variable called Delta which is an object. Delta has 3 properties. Customers(number), id(string),
+// landed(boolean).
+
+var delta = { customer: 3, id: 'user4722', landed: true }
+
+// In the program check if the number of customers is > Beta AND that delta's id is not equal to Charlie.
+
+
+// c). On a sheet or paper or digitally, draw a illustation of how following is allocated in memory
