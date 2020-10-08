8c5f55df8d05ce554b67fd14184a9e0175988ed4
DominekeB <Domineke.Beard@gmail.com>
2020-10-08 17:32:25 -0400

    New files I pulled

---
+++ intro.js
@@ -0,0 +1,30 @@
+//String
+console.log('Domineke Beard')
+
+//Number
+console.log(12345)
+
+//Boolean is a true/false
+var sunny = true
+console.log('sunny')
+//Declaration 
+
+//Initialization
+var name ='DigitalCrafts';
+console.log(typeof 'name')
+
+//Null
+
+//Undefined
+var blue 
+console.log(blue)
+
+//Object
+var car = {
+    make: 'Honda',
+    model: 'Civic',
+    Miles: 64330,
+    accident_free: false,
+    color: 'black'
+    };
+    console.log(car)
